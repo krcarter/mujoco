681cd0ce9db6e8975ad18d01f86d074c7f775eda
krcarter <kamal.carter@yahoo.com>
2023-02-11 00:38:56 -0500

    Testing Mujoco Features on Desktop

--- mujocoTest.py
+++ mujocoTest.py
@@ -1,14 +1,80 @@
-import numpy as np 
+#!/usr/bin/env python3
+
+import mujoco
+import numpy as np
+np.set_printoptions(precision=3, suppress=True, linewidth=100) 
+
+import mediapy as media
 from matplotlib import pyplot as plt 
 
-x = np.linspace(0,10, num = 100) 
-y = x ** 2 + (np.sin(100*x))**3 + 5
+# Testing Mujoco Library
 
-print(y)
+# https://colab.research.google.com/github/deepmind/mujoco/blob/main/python/tutorial.ipynb#scrollTo=eU7uWNsTwmcZ
 
-plt.title("Matplotlib demo") 
-plt.xlabel("x axis caption") 
-plt.ylabel("y axis caption") 
-plt.plot(x,y) 
-plt.show()
+xml = """
+<mujoco>
+  <worldbody>
+    <geom name="red_box" type="box" size=".2 .2 .2" rgba="1 0 0 1"/>
+    <geom name="green_sphere" pos=".2 .2 .2" size=".1" rgba="0 1 0 1"/>
+  </worldbody>
+</mujoco>
+"""
 
+model = mujoco.MjModel.from_xml_string(xml)
+
+model.ngeom
+print(model.ngeom)
+model.geom_rgba
+print(model.geom_rgba)
+
+try:
+  model.geom()
+except KeyError as e:
+  print(e)
+
+model.geom('green_sphere')
+print(model.geom('green_sphere'))
+
+model.geom('green_sphere').rgba
+print(model.geom('green_sphere').rgba)
+
+id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_GEOM, 'green_sphere')
+model.geom_rgba[id, :]
+
+print(model.geom_rgba[id, :])
+
+print('id of "green_sphere": ', model.geom('green_sphere').id)
+print('name of geom 1: ', model.geom(1).name)
+print('name of body 0: ', model.body(0).name)
+
+[model.geom(i).name for i in range(model.ngeom)]
+print([model.geom(i).name for i in range(model.ngeom)])
+
+data = mujoco.MjData(model)
+print(data)
+
+print(data.geom_xpos)
+
+mujoco.mj_kinematics(model, data)
+print('raw access:\n', data.geom_xpos)
+
+# MjData also supports named access:
+print('\nnamed access:\n', data.geom('green_sphere').xpos)
+
+# Basic rendering, simulation, and animation
+
+# Make model and data
+model = mujoco.MjModel.from_xml_string(xml)
+data = mujoco.MjData(model)
+
+# Make renderer, render and show the pixels
+mujoco.mj_forward(model, data)
+renderer = mujoco.Renderer(model)
+print(renderer)
+print('rendered object:',  renderer.__dir__())
+print(type(renderer))
+
+'''
+while True:
+    media.show_image(renderer.render())
+'''
